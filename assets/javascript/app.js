361d2ce5131eab9aa36fe864a0f2035bb3684616
joeman098 <Jlogue908@gmail.com>
2017-12-22 13:21:02 -0500

    home page added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,10 +29,16 @@ function apicall(){
 		title: response.results[i].original_title ,
 		overview: response.results[i].overview ,
 		tvtitle:response.results[i].original_name,
+		date:response.results[i].release_date,
+		tvdate:response.results[i].first_air_date,
 		}
 		// newData.text(data.title)
+	
+
+
+
 		newRow.append($("<td>").append($("<img>").attr("src","https://image.tmdb.org/t/p/w185_and_h278_bestv2/"+data.poster)))
-				.append($("<td id='title'>").text(data.title || data.tvtitle))
+				.append($("<td id='title'>").html((data.title || data.tvtitle) +"<br>"+ (data.date|| data.tvdate)))
 				.append($("<td>").attr("id","description").text(data.overview))
 				.append($("<td>").append($("<input>").attr("type","checkbox")));
 
